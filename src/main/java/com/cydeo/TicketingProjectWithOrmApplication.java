e4ff0d05512bae6e3f22985abf0c685c751c2de7
karim Abdelbaky <karim.ma7rous@yahoo.com>
2024-05-20 21:56:16 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectWithOrmApplication.java
@@ -0,0 +1,14 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectWithOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectWithOrmApplication.class, args);
+    }
+
+
+}
