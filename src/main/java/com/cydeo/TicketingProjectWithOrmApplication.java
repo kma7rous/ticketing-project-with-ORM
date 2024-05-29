a308f9c1a1d7564768841a6623377cd9825b177e
karim Abdelbaky <karim.ma7rous@yahoo.com>
2024-05-29 00:31:24 -0400

    part-2

--- src/main/java/com/cydeo/TicketingProjectWithOrmApplication.java
+++ src/main/java/com/cydeo/TicketingProjectWithOrmApplication.java
@@ -1,7 +1,9 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketingProjectWithOrmApplication {
@@ -10,5 +12,9 @@ public class TicketingProjectWithOrmApplication {
         SpringApplication.run(TicketingProjectWithOrmApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 
 }
